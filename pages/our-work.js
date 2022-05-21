8a768fa4bacaf00fbf9f7eaca14659332cacfeef
benliam <raylian2796@gmail.com>
2022-05-21 16:21:30 +0700

    Edit Our work page

--- pages/our-work.js
+++ pages/our-work.js
@@ -7,15 +7,60 @@ function OurWork(props) {
 
   return (
     <>
-    <Layout title="Our work"  header="" footer="fixed">
+    <Layout title="Our work"  header="" footer="default">
     <main>
-      <div className="jumbotron jumbotron-fluid vh-100 d-flex align-items-center">
-        <div className="container-fluid py-6 text-center">
-          <h6 className="in-left">Our work</h6>
-          <h1 className="in-top">{props.text}</h1>
-        </div>
-      </div>
-    </main>
+        <section>
+          <div className="container-fluid py-6">
+            <h1> <span className="d-block in-left">Our Work — Lightbox</span></h1>
+            <h3 className="d-block in-top">Check out more work at @benz.tattoodesign</h3>
+          </div>
+          <div className="container-fluid"> 
+            <div className="grid gallery">
+              <div className="grid-sizer col-1" /><a className="lightbox grid-item col-3" href="/images/16.jpg"><img alt="Neoto - Creative Studio Portfolio Template" src="images/16.jpg" />
+                <div className="caption p-3">
+                  <div className="title">Flourish Away</div>
+                </div>
+              </a><a className="lightbox grid-item col-3" href="/images/10.jpg"><img alt="Neoto - Creative Studio Portfolio Template" src="images/10.jpg" />
+                <div className="caption p-3">
+                  <div className="title">Suit up</div>
+                </div>
+              </a><a className="lightbox grid-item col-3" href="/images/8.jpg"><img alt="Neoto - Creative Studio Portfolio Template" src="images/8.jpg" />
+                <div className="caption p-3">
+                  <div className="title">Mix it up</div>
+                </div>
+              </a><a className="lightbox grid-item col-3" href="/images/17.jpg"><img alt="Neoto - Creative Studio Portfolio Template" src="images/17.jpg" />
+                <div className="caption p-3">
+                  <div className="title">Next-gen Shoes</div>
+                </div>
+              </a><a className="lightbox grid-item col-3" href="/images/6.jpg"><img alt="Neoto - Creative Studio Portfolio Template" src="images/6.jpg" />
+                <div className="caption p-3">
+                  <div className="title">Back to Tapes</div>
+                </div>
+              </a><a className="lightbox grid-item col-3" href="/images/7.jpg"><img alt="Neoto - Creative Studio Portfolio Template" src="images/7.jpg" />
+                <div className="caption p-3">
+                  <div className="title">Turn the page</div>
+                </div>
+              </a><a className="lightbox grid-item col-3" href="/images/15.jpg"><img alt="Neoto - Creative Studio Portfolio Template" src="images/15.jpg" />
+                <div className="caption p-3">
+                  <div className="title">Silk White</div>
+                </div>
+              </a><a className="lightbox grid-item col-3" href="/images/5.jpg"><img alt="Neoto - Creative Studio Portfolio Template" src="images/5.jpg" />
+                <div className="caption p-3">
+                  <div className="title">Branch X</div>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div className="container my-5 py-6">
+            <div className="row justify-content-md-center text-center">
+              <div className="col-sm-8">
+                <h3>We work with 100% our passion</h3>
+                <p>We strive to be as unique as possible in each project. Your tattoo will be the greatest thanks to our zeal and inventiveness.</p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
     </Layout>
   </>
   )
