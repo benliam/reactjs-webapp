9922b7955ff01af5af42e606da18d218f8f73a22
benliam <raylian2796@gmail.com>
2023-04-17 11:16:34 +0700

    Update Ourwork page

--- pages/our-work.js
+++ pages/our-work.js
@@ -4,6 +4,7 @@ import Layout from './Compontents/Layout';
 import { createClient } from '@supabase/supabase-js'
 import Image from 'next/image'
 import Head from 'next/head'
+import Script from 'next/script'
 
 function OurWork({ images }) {
 
@@ -17,15 +18,17 @@ function OurWork({ images }) {
             <h3 className="d-block in-top">Check out more work at @benz.tattoodesign</h3>
           </div>
           <div className="container-fluid"> 
-            <div className="grid gallery">
-              <div className="grid-sizer col-1" />
-              {images.map((image) => (
-              <ImageBox key={image.id} src={image.src} artist={image.artist} />
-              ))}
-            </div>
+          <figure data-behold-id="BoXO5wNF1z5dVHN4lkG0"></figure>
+          <Script src="https://w.behold.so/widget.js" type="module" strategy="afterInteractive"/>
           </div>
-          <div className="container my-5 mt-5 pt-5 py-6">
+
+          <div className="container my-5 mt-5 pt-3 py-6">
             <div className="row justify-content-md-center text-center">
+
+            <div className="col-sm-8 pb-5">
+               <a href='https://instagram.com/benz.tattoo.design' className="btn btn-primary" target="_blank">Load more</a>
+              </div>
+
               <div className="col-sm-8">
                 <h3>We work with 100% our passion</h3>
                 <p>We strive to be as unique as possible in each project. Your tattoo will be the greatest thanks to our zeal and inventiveness.</p>
@@ -39,34 +42,14 @@ function OurWork({ images }) {
   )
 }
 
-function ImageBox({src, artist}){
-  return (
-    <a className="lightbox grid-item col-3" href={src}>
-      <img alt="" layout="fill" objectFit="cover" src={src}/>
-      <div className="caption p-3">
-        <div className="title">{artist}</div>
-      </div>
-  </a>
+function PreLoading(){
+  return(
+    <div>
+      <h1>Loading</h1>
+    </div>
   )
+
 }
 
-export async function getStaticProps(){
-
-   // Configuring Supabase API 
-  const supabaseAdmin = createClient(`https://nlgyqjadljvfirahwojf.supabase.co`,`eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5sZ3lxamFkbGp2ZmlyYWh3b2pmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTQ2OTQ1NDksImV4cCI6MTk3MDI3MDU0OX0.eeNXbEzHE9Mwd3hEL43KxgtmONAxr3Gql9gCInhRjYk`)
-  
-  // Authorzing and get data from database
-  const { data } = await supabaseAdmin.from('images').select('*').order('id')
-  
-
-  // returning database
-  return {
-    props: {
-      images: data ? data : [],
-      result: "hello"
-    },
-    }
-    
-}
 
 export default OurWork
