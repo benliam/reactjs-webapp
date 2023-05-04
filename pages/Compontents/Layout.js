432719401ab2df1bc939376312c76f8584092cec
benliam <raylian2796@gmail.com>
2023-05-05 02:28:02 +0700

    Edit og:url

--- pages/Compontents/Layout.js
+++ pages/Compontents/Layout.js
@@ -26,10 +26,8 @@ function Layout (props) {
                 <meta property="og:locale" content="vi_VN" />
                 <meta property="og:title" content={!props.title ? `Benz.Tattoo Design` : props.title} ></meta>
                 <meta property="og:description" content='The Tattoo  Piercing Studio based in Da Nang, Vietnam. The best Tattoo studio located at the heart of Da Nang. We ink what you dream!'></meta>
-                <meta property="og:url" content={!props.url ? `https://wwww.benztattoo.ink/` : props.url}></meta>
                 <meta property="og:site_name" content="BENZTATTOO.ink" ></meta>
                 <meta property="og:image" content="https://i.imgur.com/YnXT3od.png" />
-                <meta property="og:image:secure_url" content="https://i.imgur.com/YnXT3od.png" ></meta>
                 <meta name="twitter:card" content="summary"></meta>
                 <meta name="twitter:title" content="BenZ.Tattoo Design - Tattoo  Piercing studio Da Nang"></meta>
                 <meta name="twitter:image" content="https://i.imgur.com/YnXT3od.png" ></meta>
