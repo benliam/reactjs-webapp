f987ea562188b3718a1d799f1c619bc094933999
benliam <raylian2796@gmail.com>
2023-06-12 14:16:15 +0700

    Edit index file

--- pages/_app.js
+++ pages/_app.js
@@ -61,19 +61,6 @@ function MyApp({ Component, pageProps }) {
       `,
       }}/>
 
-<Script
-      strategy="beforeInteractive"
-      id={3}
-      dangerouslySetInnerHTML={{
-        __html: `
-        (function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
-          .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
-          n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
-          (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
-          ml('account', '467061');
-      `,
-      }}/>
-
 
     </>
   )
