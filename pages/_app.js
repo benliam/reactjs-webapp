464bc6a9faec811c6ec42177c9e286948d2daee6
benliam <raylian2796@gmail.com>
2024-01-11 16:53:08 +0700

    Remove facebook chat plugin

--- pages/_app.js
+++ pages/_app.js
@@ -18,11 +18,6 @@ function MyApp({ Component, pageProps }) {
         <meta name="theme-color" content="#999999"></meta>
     </Head>
     <Component {...pageProps} />
-    <div id="fb-root"></div>
-        <div className="fb-customerchat"
-          attribution='setup_tool'
-          page_id="263952664181437">
-      </div>
     <Script src="js/main.bundle.js" strategy="beforeInteractive"/>
     <Script async src="https://www.googletagmanager.com/gtag/js?id=G-FDRP593V6T" strategy="afterInteractive"/>
     <Script
@@ -37,30 +32,6 @@ function MyApp({ Component, pageProps }) {
         gtag('config', 'G-FDRP593V6T');
       `,
       }}/>
-
-    <Script
-      id={2}
-      strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `
-        window.fbAsyncInit = function() {
-          FB.init({
-            xfbml            : true,
-            version          : 'v9.0'
-          });
-        };
-
-        (function(d, s, id) {
-        //if(document.body.clientWidth<769) return;
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
-      `,
-      }}/>
-
 
     </>
   )
